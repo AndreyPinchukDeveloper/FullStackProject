6c67be3e8dd05c27f2af44806eb719809227c199
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-11 22:33:14 +0700

    dev update
    - added nuget packages;
    - created startup class.

---
+++ BugPorter.API/BugPorter.API/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugPorter.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HelloWorld
+    {
+        private readonly ILogger<HelloWorld> _logger;
+
+        public HelloWorld(ILogger<HelloWorld> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
