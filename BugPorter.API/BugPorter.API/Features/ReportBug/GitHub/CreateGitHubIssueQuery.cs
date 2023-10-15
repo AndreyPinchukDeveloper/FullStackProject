5706bca1008b71c96cf6096d000222af7fafb48c
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-15 16:56:54 +0700

    dev update
    - connected github token to create new issues.

--- BugPorter.API/BugPorter.API/Features/ReportBug/GitHub/CreateGitHubIssueQuery.cs
+++ BugPorter.API/BugPorter.API/Features/ReportBug/GitHub/CreateGitHubIssueQuery.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +10,30 @@ namespace BugPorter.API.Features.ReportBug.GitHub
 {
     public class CreateGitHubIssueQuery
     {
+        private readonly GitHubClient _gitHubClient;
         private readonly ILogger<CreateGitHubIssueQuery> _logger;
 
-        public CreateGitHubIssueQuery(ILogger<CreateGitHubIssueQuery> logger)
+        public CreateGitHubIssueQuery(ILogger<CreateGitHubIssueQuery> logger, GitHubClient gitHubClient)
         {
             _logger = logger;
+            _gitHubClient = gitHubClient;
         }
 
         public async Task<ReportedBug> Execute(NewBug newBug)
         {
             _logger.LogInformation("Creating GitHub issue");
-            ReportedBug reportedBug = new ReportedBug("1", newBug.Summary, newBug.Description);
-            _logger.LogInformation("Succesfully created GitHub issue {id}", reportedBug.Id);
-            return reportedBug;
+
+            NewIssue newIssue = new NewIssue(newBug.Summary)
+            {
+                Body = newBug.Description
+            };
+            Issue createdIssue = await _gitHubClient.Issue.Create("Andre", "bugPorter", newIssue);
+
+            _logger.LogInformation("Succesfully created GitHub issue {number}", createdIssue.Number);
+            return new ReportedBug(
+                createdIssue.Number.ToString(),
+                createdIssue.Title,
+                createdIssue.Body);
         }
     }
 }
--- BugPorter.API/BugPorter.API/Startup.cs
+++ BugPorter.API/BugPorter.API/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using BugPorter.API.Features.ReportBug.GitHub;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,11 @@ namespace BugPorter.API
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            string gitHubToken = builder.GetContext().Configuration.GetValue<string>("GITHUB_TOKEN");
+            builder.Services.AddSingleton(new GitHubClient(new ProductHeaderValue("BugPorter-api"))
+            {
+                Credentials = new Credentials(gitHubToken)
+            });
             builder.Services.AddSingleton<CreateGitHubIssueQuery>();
         }
     }
